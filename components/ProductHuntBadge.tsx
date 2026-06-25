c97b4f23799c2d5703ed99d434931de35ece403e
pranav412-code <pr.gupta2003@gmail.com>
2026-06-26 02:56:56 +0530

    chore: add missing ProductHuntBadge component

---
+++ components/ProductHuntBadge.tsx
@@ -0,0 +1,18 @@
+export function ProductHuntBadge() {
+  return (
+    <a 
+      href="https://www.producthunt.com/products/resumecraft-6?embed=true&utm_source=badge-featured&utm_medium=badge&utm_campaign=badge-resumecraft-7" 
+      target="_blank" 
+      rel="noopener noreferrer"
+      style={{ display: "inline-block" }}
+    >
+      <img 
+        src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=1179034&theme=dark&t=1782388477113" 
+        alt="ResumeCraft - Boost your resume with AI to land more interviews | Product Hunt" 
+        style={{ width: "250px", height: "54px" }} 
+        width="250" 
+        height="54" 
+      />
+    </a>
+  );
+}
